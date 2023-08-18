44c96cf6333f633895adf1ed61a72f949d5147ea
jbahoquez <jbahoquez@gmail.com>
2023-08-18 08:40:25 -0500

    class transformer and class validator

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,10 +1,13 @@
-import { USER_ROLE } from "../entity/user.entity";
+import { IsString, IsEmail, IsNumberString } from 'class-validator';
+import { USER_ROLE } from '../entity/user.entity';
 
-
-export class CreateUserDto{
-    
-    name:  string;
-    email: string;
-    phone: number;
-    role:  USER_ROLE;
+export class CreateUserDto {
+  @IsString()
+  name: string;
+  @IsEmail()
+  email: string;
+  @IsNumberString()
+  phone: number;
+  @IsString()
+  role: USER_ROLE;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,39 +1,49 @@
-import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
-import {UserService} from './user.service'
-import {User, USER_ROLE} from './entity/user.entity'
-import {CreateUserDto} from './dto/create-user.dto'
-
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+  HttpCode,
+  HttpStatus,
+  BadRequestException,
+  ForbiddenException,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { User, USER_ROLE } from './entity/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
+  constructor(private _userService: UserService) {}
 
-    constructor(private _userService: UserService){}
+  @Get()
+  getUsers(): User[] {
+    return this._userService.getUsers();
+  }
 
-    @Get()
-    getUsers(): User[]{
-       return this._userService.getUsers()
-    }
+  @Post()
+  addUser(@Body() body: CreateUserDto): User {
+    return this._userService.createUser(body);
+  }
 
-    @Post()
-    addUser(@Body() body:CreateUserDto): User{
-        return this._userService.createUser(body)
-    }
+  @Put('/:id')
+  //@HttpCode(HttpStatus.NO_CONTENT)
+  updateUser(@Body() body: CreateUserDto, @Param('id') id: string): User {
+    const user = this._userService.getUserById(id);
+    if (!user) throw new BadRequestException('User not found');
+    //if(!user) throw new ForbiddenException('User not found');
+    return this._userService.updateUser(id, body);
+  }
 
-    @Put('/:id')
-    updateUser(@Body() body: CreateUserDto, @Param('id') id: string) : User{
-        const user = this._userService.getUserById(id);
-        if(!user) return;
-        return this._userService.updateUser(id,body);
-            
-        
-    }
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    const user = this._userService.getUserById(id);
+    if (!user) throw new BadRequestException('User not found');
 
-    @Delete('/:id')
-    deleteUser(@Param('id') id: string){
-       const user = this._userService.getUserById(id);
-       if(user){
-            this._userService.deleteUser(id);
-            return 'Usuario eliminado'
-        }
-    }
+    this._userService.deleteUser(id);
+    return 'Usuario eliminado';
+  }
 }
